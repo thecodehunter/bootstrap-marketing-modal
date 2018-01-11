4120e4cf59d0eed3c1a633890bcdc862dd81df92
Martin Sagastume <martin.sagastume@wework.com>
2018-01-11 18:25:38 -0300

    initial commit

--- src/bootstrap.marketing.modal.ts
+++ src/bootstrap.marketing.modal.ts
@@ -6,20 +6,32 @@ enum DisplayMode {
     MONTHLY = 'monthly'
 }
 
+enum ModalAction {
+    CLOSE = 'none',
+    OPEN = 'block'
+}
+
 interface MarketingModalOptions {
     modalId: string;
     onPageLoad: boolean;
     displayMode: DisplayMode;
 }
 
-class DateTimeFactory {
-    static create(datetime?) {
+class DateTimeUtils {
+    /**
+     * Get time representation from a given date or from today.
+     * @param datetime 
+     */
+    static getTime(datetime?) {
         return datetime ? new Date(Number(datetime)).setHours(0,0,0,0) : new Date().setHours(0,0,0,0);
     }
-}
 
-class ScheduleDateTimeFactory {
-    static create(displayMode?: DisplayMode): Date {
+    /**
+     * Get schedule time for a given display mode.
+     * @param displayMode
+     * @returns Date
+     */
+    static getScheduleTime(displayMode?: DisplayMode): Date {
         const today = new Date();
         let scheduleDateTime = null;
         
@@ -35,6 +47,7 @@ class ScheduleDateTimeFactory {
 
         return scheduleDateTime;
     }
+
 }
 
 class MarketingModal {
@@ -53,6 +66,9 @@ class MarketingModal {
         this.executeActions();
     }
 
+    /**
+     * Handle display=unique cases
+     */
     private uniqueDisplayModeHandler(): void {
         const unique = Number(localStorage.getItem(this.internalId)) === 0;
         if (!unique) {
@@ -60,32 +76,40 @@ class MarketingModal {
             this.open();
         }
     }
-
+    /**
+     * Handle scheduled day cases 
+     */
     private scheduleDisplayModeHandler(): void {
 
         const currentScheduleDateTime = localStorage.getItem(this.internalId);
+
         // first time schedule or previously set to unique 
         if (!currentScheduleDateTime || currentScheduleDateTime === '0') {
             this.saveScheduleDateTime();
             return;
         }
 
-        const currentScheduleDate = DateTimeFactory.create(currentScheduleDateTime);
+        const currentScheduleDate = DateTimeUtils.getTime(currentScheduleDateTime);
 
-        if (currentScheduleDate && currentScheduleDate === DateTimeFactory.create()) {
+        if (currentScheduleDate && currentScheduleDate === DateTimeUtils.getTime()) {
             this.open();
             this.saveScheduleDateTime();
         }
     }
-
+    /**
+     * Save the scheduled date time on localStorage
+     */
     private saveScheduleDateTime(): void {
-        const newScheduleDateTime = ScheduleDateTimeFactory.create(this.options.displayMode);
+        const newScheduleDateTime = DateTimeUtils.getScheduleTime(this.options.displayMode);
 
         if (newScheduleDateTime) {
             localStorage.setItem(this.internalId, newScheduleDateTime.toString());
         }
     }
 
+    /**
+     * Executes the corresponding actions for each mode config.
+     */
     private executeActions(): void {
         if (this.options.onPageLoad) {
             if (this.options.displayMode === DisplayMode.UNIQUE) {
@@ -98,17 +122,29 @@ class MarketingModal {
         }
     }
 
+    /**
+     * Register the event listeners for the modal.
+     * We provide a close event on modal dismiss action to allow a they user to close the modal without
+     * using jQuery or depending on it.
+     */
     private registerEventListeners(): void {
         document.querySelector(`#${this.options.modalId} [data-dismiss="modal"]`).addEventListener("click", () => {
             this.close();
         });
     }
 
+    /**
+     * Provides a way to manually open the current modal
+     */
     public open(): void {
-        this.modalDomRef.style.display = 'block';
+        this.modalDomRef.style.display = ModalAction.OPEN;
     }
+    
+    /**
+     * Provides a way to manually close the current modal
+     */
     public close(): void {
-        this.modalDomRef.style.display = 'none';
+        this.modalDomRef.style.display = ModalAction.CLOSE;
     }
 
 }
